c645e864c1297a0132e95b95cd271a4108f4e715
Carinalak <carina.lakosil@gmail.com>
2023-11-18 10:27:06 +0100

    planering och bilder

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 /*
 1. en meny knapp kanske
-2. 
+2. Sida 1: "salespage". En plus och minusknapp, antal, pris, bild.
+3. Sida 2: "open-item-page".
+4. Sida 3: "Shopping-cart-page". Trashbin,
 
 */
 
