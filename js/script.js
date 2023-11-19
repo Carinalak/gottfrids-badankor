a2c9863f35110802a84bb6b70956bc5ecc15db31
Carinalak <carina.lakosil@gmail.com>
2023-11-19 11:44:21 +0100

    övar på plus-minus-knapp

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 /*
-1. en meny knapp kanske
+1. en fungerande menyknapp kanske
 2. Sida 1: "salespage". En plus och minusknapp, antal, pris, bild.
 3. Sida 2: "open-item-page".
-4. Sida 3: "Shopping-cart-page". Trashbin,
+4. Sida 3: "Shopping-cart-page". Behöver Trashbin, formulär till beställarens uppgifter.
 
 */
 
 document.querySelector('#list').innerHTML = '<ul><li>Test</li></ul>';
 document.querySelector('#imageContainer').innerHTML = '<img src="img/badring.jpg" width="250" height="250">';
+
+
+
+
+document.querySelector('#imageContainer2').innerHTML = '<img src="img/affarskvinna.jpg" width="250" height="250">';
+
