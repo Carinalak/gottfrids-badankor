6444042869ea5ae36ce3687f10602bdca5b1bcc2
Carinalak <carina.lakosil@gmail.com>
2023-11-20 16:44:44 +0100

    jobbar med förstasidan

--- js/script.js
+++ js/script.js
@@ -5,7 +5,67 @@
 4. Sida 3: "Shopping-cart-page". Behöver Trashbin, formulär till beställarens uppgifter.
 
 */
+const duckies = [
+    {
+        name: 'Anka med badring',
+        price: 29,
+    },
+    {
+        name: 'Affärskvinna',
+        price: 39,
+    },
+    {
+        name : 'Dark duck',
+        price: 39,
+    },
+];
+const duckContainer = document.querySelector('#duckies');
 
+duckContainer.innerHTML = `<div>${duckies[0].name}</div>`
+
+// Här börjar koden från gamla plusknapparna:
+
+const increaseBtn = document.querySelector('#increase');        // väljer id:t increase knappen
+const decreaseBtn = document.querySelector('#decrease');        // väljer id:t decrease knappen
+const startAmount = document.querySelector('#amount');
+const price = document.querySelector('#price');
+let amount = Number(startAmount.value);                               // Genom att skriva "Number" framför tvingas strängen att bli en siffra.
+
+
+increaseBtn.addEventListener('click', increaseAmount);          // Event - lyssnare med 'click' på increaseBtn
+decreaseBtn.addEventListener('click', decreaseAmount); 
+startAmount.addEventListener('change', updateAmount);
+function updateAmount(e) {
+    amount = Number(e.target.value);
+    updatePrice();
+}
+function increaseAmount() {                                     // En funktion till event - lyssnaren
+    amount += 1;
+    
+
+    updatePrice();                                              // functionen anropas   
+}
+
+function decreaseAmount() {                                     // En funktion till event - lyssnaren
+    if (amount - 1 < 0 ) {                                      // Om amount är mindre än noll så:
+        return;                                                 // gör en return = efter return körs inte koden vidare.
+    }
+    amount -= 1;
+    
+
+    updatePrice();                                              // functionen anropas   
+}
+
+
+function  updatePrice() {       
+    startAmount.value = amount;                                // gör en funktion som anropas ovan.
+    price.innerHTML = amount * 15;
+}
+
+
+
+
+/*
 document.querySelector('#list').innerHTML = '<ul><li>Test</li></ul>';
 
 //document.querySelector('#imageContainer').innerHTML = '<img src="img/affarskvinna.jpg" width="250" height="250">';
@@ -15,3 +75,8 @@ document.querySelector('#list').innerHTML = '<ul><li>Test</li></ul>';
 
 document.querySelector('#imageContainer2').innerHTML = '<img src="img/badring.jpg" width="250" height="250" alt="Anka med badring">';
 
+const ducks = ['Anka med badring', 'Affärskvinna', 'Snorklare', 'Dark Duck', 'Examen', 'Drottning'];
+console.log(ducks.length);
+
+const duckSorts = document.querySelector('#duckList');
+*/
